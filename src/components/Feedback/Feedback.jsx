eea56f6f1e3beaac48f73e74fa0940b1ef59af2e
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-06-29 21:14:29 -0500

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,11 @@
-export default function Feedback({ feedback,totalFeedback }) {
+export default function Feedback({ feedback:{good, neutral, bad}, totalFeedback }) {
   return (
     <div>
-      <p>Good: {feedback.good}</p>
-      <p>Neutral: {feedback.neutral}</p>
-      <p>Bad: {feedback.bad}</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
       <p>Total: {totalFeedback}</p>
-      <p>Positive: {Math.round((feedback.good / totalFeedback) * 100) }%</p>
+      <p>Positive: {Math.round((good / totalFeedback) * 100) }%</p>
     </div>
   )
 }
